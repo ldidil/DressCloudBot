bfe6a5894d3f7e18dd368090c51bd5b47c042359
ldidi <dominila.l@gmail.com>
2021-02-28 18:16:34 +0100

    Create Project

--- DressCloudBot/DCbot.cs
+++ DressCloudBot/DCbot.cs
@@ -13,7 +13,11 @@ namespace DressCloudBot
     {
         public IWebDriver Driver;
         string basePage = "https://dresscloud.pl/";
+        readonly string login;
+        readonly string password;
 
+        int numberStartPage;
+        int numberStopPage;
         public object SeleniumExtras { get; private set; }
 
         [SetUp]
@@ -21,20 +25,19 @@ namespace DressCloudBot
         {
             Driver = new ChromeDriver();
             Driver.Navigate().GoToUrl(basePage);
-            Driver.FindElement(By.CssSelector("#loginarea > span.show-login-form.inline-block.pointer.hover-fade")).Click();
-            Driver.FindElement(By.Id("id_username")).SendKeys("ldidil");
-            Driver.FindElement(By.Id("id_password")).SendKeys("d98d98");
-            Driver.FindElement(By.Id("loginbutton")).Click();
-            //Thread.Sleep(7000);
-            //Driver.FindElement(By.CssSelector("body > div.app_gdpr--SPx19r > div.popup_popup--1TXMW > div.popup_content--2JBXA > div > div.details_footer--1oDeu > button.button_button--lgX0P.details_save--1ja7w > b > span")).Click();
 
+            Driver.FindElement(By.CssSelector("#loginarea > span.show-login-form.inline-block.pointer.hover-fade")).Click();
+            Driver.FindElement(By.Id("id_username")).SendKeys(login);
+            Driver.FindElement(By.Id("id_password")).SendKeys(password);
+            Driver.FindElement(By.Id("loginbutton")).Click();
+       
         }
 
         [Test]
-        public void Krysztalkuj()
+        public void Test()
         {
 
-            for (int i = 3099; i < 3105; i++)
+            for (int i = numberStartPage; i < numberStopPage; i++)
             {
 
                 Driver.Navigate().GoToUrl($"{basePage}cloud/?strona={i}");
@@ -43,7 +46,6 @@ namespace DressCloudBot
 
                 foreach (IWebElement e in cristals)
                 {
-
                     //Thread.Sleep(200);
                     IsClickable(e);
                 }
@@ -51,33 +53,17 @@ namespace DressCloudBot
             Driver.Close();
         }
 
-        //[Test]
-        //public void Aktywnosc()
-        //{
-        //    Driver.Navigate().GoToUrl($"{basePage}cloud/ubrania/suknie-sukienki/713679-sukienka-asos/");
-        //    Thread.Sleep(3000);
-        //    Driver.FindElement(By.CssSelector("body > div.page > div.photos_stage.sp-init > div.ps_outer > div.ps_inner.vert-middle > div > div.ps_popup > div.ps_imgstage > div.ps_thumbs > div:nth-child(4) > a > span")).Click();
-        //    Thread.Sleep(5000);
-
-        //    for (int i = 0; i <50; i++)
-        //    {
-
-               
-        //        Driver.FindElement(By.CssSelector("#videoDiv > button")).Click();
-        //        Thread.Sleep(4000);
-
-        //    }
-        //    Driver.Close();
-        //}
 
         public void ScrollPage(IWebElement element)
         {
             IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
             js.ExecuteScript("arguments[0].scrollIntoView();", element);
         }
+
+        //auxiliary function, used at the stage of code creation 
+        //draw a border around the found element
         public void BorderElement(IWebElement elem)
         {
-            // draw a border around the found element
             IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
             js.ExecuteScript("arguments[0].style.border='3px solid red'", elem);
 
